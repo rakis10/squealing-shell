adc3910f047f8ad1697b100bb7d670af5490486f
rakis10 <jurajrak14@gmail.com>
2024-12-10 01:50:50 +0100

    tailwind astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://rakis10.github.io',
     base: 'squealing-shell',
+    // add css file
+    integrations: [tailwind()],
 });
